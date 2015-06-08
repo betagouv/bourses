df7d26c389a22f0bfcd9ca6464729f2f064be71e
Florian Delezenne <del.florian@gmail.com>
2015-06-08 10:16:01 +0200

    Fix tests

---
+++ client/app/formulaire/identite-enfant/identite-enfant.cntroller.spec.js
@@ -0,0 +1,38 @@
+'use strict';
+
+describe('Controller: partenaire', function () {
+
+  // load the service's module
+  beforeEach(module('boursesApp'));
+
+  it('should render initial data', function () {
+    //given
+    var scope = {};
+    var store = {
+      steps: {},
+      set: function(str, val) {
+        this[str] = val;
+      },
+      get: function(str) {
+        return this[str];
+      }
+    };
+
+    //when
+    inject(function($controller){
+      $controller('IdentiteEnfantCtrl', {
+        $scope: scope,
+        store: store
+      });
+    });
+
+    scope.submit({
+      $valid: true
+    });
+
+    //then
+    expect(store.steps.identiteEnfant).toBeDefined();
+
+  });
+
+});
