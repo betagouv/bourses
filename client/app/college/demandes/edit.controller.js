421c0a5d3ba311ad625604fc843df2336af30546
Florian Delezenne <del.florian@gmail.com>
2015-06-26 16:14:17 +0200

    Fix download de pdf

--- client/app/college/demandes/edit.controller.js
+++ client/app/college/demandes/edit.controller.js
@@ -1,9 +1,10 @@
 'use strict';
 
 angular.module('boursesApp')
-  .controller('DemandeEditCtrl', function($scope, $timeout, $http, demandeId, demande) {
+  .controller('DemandeEditCtrl', function($scope, $timeout, $http, Auth, demandeId, demande) {
     $scope.demande = demande;
     $scope.demandeId = demandeId;
+    $scope.token = Auth.getToken();
     $scope.save = function() {
       $scope.saving = 'pending';
       $http.post('/api/demandes/comment/' + demandeId, {observations: demande.observations}).then(function() {
