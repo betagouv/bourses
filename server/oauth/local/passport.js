ed918defaba2f2e9bd550590852e84db9d151461
Florian Delezenne <del.florian@gmail.com>
2015-06-29 11:13:24 +0200

    Affichage de l'erreur sur login incorrect

--- server/oauth/local/passport.js
+++ server/oauth/local/passport.js
@@ -13,10 +13,10 @@ exports.setup = function (User, config) {
         if (err) return done(err);
 
         if (!user) {
-          return done(null, false, { message: 'This email is not registered.' });
+          return done(null, false, { message: 'Email ou mot de passe incorrect' });
         }
         if (!user.authenticate(password)) {
-          return done(null, false, { message: 'This password is not correct.' });
+          return done(null, false, { message: 'Email ou mot de passe incorrect' });
         }
         return done(null, user);
       });
