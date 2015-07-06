2b5b0d91d244341776c9a333a8b17e1e45973093
Florian Delezenne <del.florian@gmail.com>
2015-07-06 15:26:47 +0200

    Refonte affichage du résultat de simulation

--- client/components/string-utils/string-utils.filters.js
+++ client/components/string-utils/string-utils.filters.js
@@ -28,7 +28,6 @@ angular.module('boursesApp')
       switch (input) {
         case 'externe':
           return 'Non';
-        case 'interne':
         default:
           return 'Oui';
       }
@@ -43,6 +42,31 @@ angular.module('boursesApp')
           return 'Mère';
         default:
           return 'Responsable légal';
+      }
+    };
+  })
+  .filter('montant', function () {
+    return function (input) {
+      switch (input) {
+        case null:
+          return 'Pas de résultat';
+        case 0:
+          return 'Vous n\'avez pas droit à une bourse';
+        default:
+          return input + ' euros';
+      }
+    };
+  })
+  .filter('descriptionMontant', function () {
+    return function (input) {
+      switch (input) {
+        case null:
+          return 'Résultat de votre simulation';
+        case 0:
+          return 'D\'après les informations saisies , votre enfant ne pourra pas bénéficier d\'une bourse de collège.';
+        default:
+          return 'D\'après les informations saisies, ' +
+            'votre enfant pourra bénéficier d\'une bourse de collège d\'un montant annuel de:';
       }
     };
   });
