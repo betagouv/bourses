fa1558b9e11e190ff15ac220629388b4c678c28e
Florian Delezenne <del.florian@gmail.com>
2015-08-27 18:00:04 +0200

    Ajout message experimentation

--- client/app/layout/layout.js
+++ client/app/layout/layout.js
@@ -5,6 +5,19 @@ angular.module('boursesApp')
     $stateProvider
       .state('layout', {
         url: '',
+        controller: function($scope, store, $timeout) {
+          angular.element(document).ready(function() {
+            $timeout(function() {
+              $scope.showIntro = !store.get('hideIntro');
+            }, 500);
+          });
+
+          $scope.hideIntro = function() {
+            $scope.showIntro = false;
+            store.set('hideIntro', true);
+          };
+        },
+
         templateUrl: 'app/layout/layout.html',
         abstract: true
       });
