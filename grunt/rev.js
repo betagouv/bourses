0d645b34b50e92d41438ed1bdafab5f58068ddbf
Florian Delezenne <del.florian@gmail.com>
2015-08-31 18:34:31 +0200

    Fix grunt

--- grunt/rev.js
+++ grunt/rev.js
@@ -6,7 +6,10 @@ module.exports = {
         'dist/{,*/}*.css',
         'dist/assets/fonts/*',
         '!dist/server/*',
-        '!dist/bower_components/spin.js'
+        '!dist/bower_components/spin.js',
+        '!dist/bower_components/Chart.js',
+        '!dist/bower_components/angular-chart.js',
+        '!dist/bower_components/Chart.StackedBar.js'
       ]
     }
   }
