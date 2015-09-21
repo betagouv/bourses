5b8acead396ae41440abe245c37ea06d1c8b5063
Florian Delezenne <del.florian@gmail.com>
2015-09-21 11:27:53 +0200

    Fix pagination

--- client/app/college/demandes/list.controller.js
+++ client/app/college/demandes/list.controller.js
@@ -13,9 +13,8 @@ angular.module('boursesApp')
     Etablissement.queryDemandes({id: id, status: status, searchQuery: {
       q: recherche,
       offset: (currentPage - 1) * 10
-    }}, function(demandes, getResponseHeaders) {
+    }}, function(demandes) {
       $scope.demandes = demandes;
-      $scope.totalItems = getResponseHeaders('count');
     });
 
     $scope.search = function(recherche) {
