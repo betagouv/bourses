80f5719db8e380c33712f88437aac7b81d8c1495
Florian Delezenne <del.florian@gmail.com>
2015-09-25 15:43:10 +0200

    Ajout gestion de l'annee d'avis

--- client/app/college/demandes/create/create.controller.js
+++ client/app/college/demandes/create/create.controller.js
@@ -12,14 +12,19 @@ angular.module('boursesApp')
         concubinage: 'non'
       },
       data: {
-        anneeImpots: '2014',
-        anneeRevenus: '2013'
-      },
+        anneeImpots: 2014,
+        anneeRevenus: 2013
+      }
     };
 
     $scope.submit = function(form) {
       if (!form.$valid) {
         return;
+      }
+
+      $scope.demande.data.anneeRevenus = $scope.demande.data.anneeImpots - 1;
+      if ($scope.demande.data_conjoint) {
+        $scope.demande.data_conjoint.anneeRevenus = $scope.demande.data_conjoint.anneeImpots - 1;
       }
 
       $http.post('/api/demandes/' + college._id, $scope.demande).then(function() {
