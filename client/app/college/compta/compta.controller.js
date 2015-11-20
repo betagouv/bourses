bd7bda975a74f968f7ef9c1ddd3260d22d4f7362
Florian Delezenne <del.florian@gmail.com>
2015-11-20 11:17:06 +0100

    Add bic search

--- client/app/college/compta/compta.controller.js
+++ client/app/college/compta/compta.controller.js
@@ -2,13 +2,21 @@
 'use strict';
 
 angular.module('boursesApp')
-  .controller('ComptaCollegeCtrl', function($scope, $http, Auth, id, college) {
+  .controller('ComptaCollegeCtrl', function($scope, $http, $window, Auth, id, college) {
     $scope.token = Auth.getToken();
     $scope.college = college;
 
     $http.get('api/etablissements/' + id + '/compta').then(function(result) {
       $scope.demandes = result.data;
     });
+
+    $scope.search = function(bic) {
+      if (bic.length === 11 && bic.slice(-3) === 'XXX') {
+        bic = bic.slice(0, 8);
+      }
+
+      $window.open('//www.theswiftcodes.com/france/' + bic + '/#admiddlebottom');
+    };
 
     $scope.update = function(demande, attr) {
       return $http.put('/api/demandes/' + demande._id, {attr: 'data.identiteAdulte.' + attr, value: demande.identiteAdulte[attr]});
