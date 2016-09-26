fa7427aad39ef5b122234613f2ee18a5f720b2b8
Florian Delezenne <del.florian@gmail.com>
2016-09-26 15:13:48 +0200

    Add links to menu

--- client/app/college/college.controller.js
+++ client/app/college/college.controller.js
@@ -3,6 +3,8 @@
 angular.module('boursesApp')
   .controller('CollegeAbstractCtrl', function($scope, $http, $state, Auth, college, count) {
     $scope.count = count;
+    $scope.college = college;
+    $scope.token = Auth.getToken();
 
     function updateCount() {
       return $http({method: 'GET', url: '/api/etablissements/' + college.human_id + '/count'}).then(function(result) {
