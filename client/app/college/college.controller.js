3f161cbf2f591bba93472a30aff45a6a0c61846b
Florian Delezenne <del.florian@gmail.com>
2016-10-10 16:04:28 +0200

    Afficher l'aide pour la fermeture du service

--- client/app/college/college.controller.js
+++ client/app/college/college.controller.js
@@ -1,10 +1,17 @@
 'use strict';
 
 angular.module('boursesApp')
-  .controller('CollegeAbstractCtrl', function($scope, $http, $state, Auth, college, count) {
+  .controller('CollegeAbstractCtrl', function($scope, $http, $state, Auth, store, college, count) {
     $scope.count = count;
     $scope.college = college;
     $scope.token = Auth.getToken();
+
+    $scope.hideHelpCampaign = store.get('hideHelpCampaign');
+
+    $scope.hideHelp = function() {
+      $scope.hideHelpCampaign = true;
+      store.set('hideHelpCampaign', $scope.hideHelpCampaign);
+    };
 
     function updateCount() {
       return $http({method: 'GET', url: '/api/etablissements/' + college.human_id + '/count'}).then(function(result) {
